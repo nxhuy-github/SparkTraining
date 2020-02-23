aaa5cf0269b1905ff0da2322445f1772dcc688e0
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2020-02-23 01:13:55 +0100

    push

---
+++ src/main/java/com/nxhuy/spark/training/SparkTraining/App.java
@@ -0,0 +1,13 @@
+package com.nxhuy.spark.training.SparkTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
